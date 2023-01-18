d6db70928bd51a676968cf7457f595b7d08fd526
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-18 13:37:08 +0000

    feat: create comment model
    1. Create post model.
    2. order by created on date.
    3. Return string with comment body & post creator name.

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -32,5 +32,20 @@ class Post(models.Model):
         return self.likes.count()
 
 
+class Comment(models.Model):
 
-# Create your models here.
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.name}'
