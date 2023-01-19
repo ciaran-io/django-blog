148d0a27959a0162fc4a76462dc34c20c846c725
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-01-19 13:39:57 +0000

    feat: disable debug for production, setup cors  x frame options

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['localhost', os.environ.get('PRODUCTION_HOSTNAME')]
 
 
